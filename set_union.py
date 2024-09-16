2d9b79560993c80a57c3da2f8e09e9890de2ec2e
Raghs3 <tinku.minku2803@gmail.com>
2024-09-16 06:56:13 +0530

    adding files

---
+++ set_union.py
@@ -0,0 +1,14 @@
+from prescription_data import adverse_interactions
+
+meds_to_watch = set()
+
+# for interaction in adverse_interactions:
+#     # meds_to_watch = meds_to_watch.union(interaction)
+#     # meds_to_watch = meds_to_watch | interaction  # union
+#     # meds_to_watch.update(interaction)
+#     meds_to_watch |= interaction  # union_update
+
+meds_to_watch.update(*adverse_interactions)  # unpacking list
+
+# print(sorted(meds_to_watch))
+print(*sorted(meds_to_watch), sep="\n")  # unpacking list, sorted produces list
